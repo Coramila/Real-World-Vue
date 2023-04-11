aac258f773ef00c8a4a7093e923681f6965fb534
Camila Carvalho <carvalho.camiilaa@gmail.com>
2023-04-11 10:41:12 -0300

    Event card

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,14 +1,31 @@
 <script setup>
+import { ref } from 'vue'
 // defineProps({
 //   msg: {
 //     type: String,
 //     required: true
 //   }
 // })
+
+const event = ref ({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline frient at this event',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee',
+})
 </script>
 
 <template>
-  <div class="event-card"></div>
+  <div class="event-card"> 
+    <h2> {{ event.title }}</h2>
+    <span>@{{  event.time }} on {{ event.date }}</span>
+</div>
+ 
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import EventeCard from '@/components/EventCard.vue'
 
 </script>
 
 <template>
   <div class="home"></div>
+  <EventeCard/>
 </template>
 
 
